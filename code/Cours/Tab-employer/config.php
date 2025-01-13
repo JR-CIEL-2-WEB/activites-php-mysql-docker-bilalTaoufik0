314aca78e70524f3387883e3ea6010171da9a3a7
Bilal TAOUFIK <144660186+bilalTaoufik0@users.noreply.github.com>
2025-01-13 17:04:41 +0000

    save

--- code/Cours/Tab-employer/config.php
+++ code/Cours/Tab-employer/config.php
@@ -1,13 +1,15 @@
 <?php
-$host = '127.0.0.1';
-$dbname = 'appdb'; // Remplacez par votre nom de base de données
-$username = 'eleve';
-$password = 'eleve';
+$host = '127.0.0.1'; // or 'localhost'
+$port = '8081';
+$dbname = 'appdb';
+$username = 'root';
+$password = 'root';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
+
 ?>
